1c0c2b69d4bd5db9866522968af56456f1e6d692
Greg Lanter <dohnut22.gl@gmail.com>
2019-04-28 13:01:00 -0700

    links open in new tab

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,29 +1,40 @@
+debugger;
+if (key==null){
+    var key = new Date();
+    alert(key);
+}else{
+    var screen = localStorage.getItem(key);
+    if (screen==null){
+        screen="greg"
+    }
+}
+
 var parent_div = $("<div>");
 var top_button_holder = $('<div class="button-holder">');
 var top_button = $('<i class="far fa-keyboard light-text clear"></i>');
 var section_title = $('<div class="section-title clear">');
 var top_header = $('<h2 class="clear">Experience</h2>');
 var top_div = $('<div class="col-md-12 bio">');
-var top_text = "Hi! I'm a long time developer and a new web developer. I have been programming for 19 years now. I enjoy the puzzle aspect of programming. It may start a while to get started but once into a issue I quickly get engrossed in solving the problem. I have work in all kinds of offices from big corporate place to smal shops of less than 10 programmers. One of my favorite projects was customing and inatlling barcode software in a warehouse shipping area.";
+var top_text = "";
 
 var mid_button_holder = $('<div class="button-holder">');
 var mid_button = $('<i class="fas fa-database light-text clear icon=pro"></i>');
 var mid_title = $('<div class="section-title">');
 var mid_header = $('<h2 class="clear">Professional Skills</h2>');
 var mid_holder = $('<div class="col-md-12 bio">');
-var mid_text = "I am the youngest of 7 kids and have a wife and kids of my own. I love watching hockey and The Simpsons. The hockey teams I follow are the St. Louis Blues and the Everett Silvertips. I enjoy traveling I have been to England, Scotland, Italy, Isreal, Jordan and the Philippines and I am planning on going to Germany in 2020!";
+var mid_text = "";
 
 var bottom_button_holder = $('<div class="button-holder">');
 var bottom_button = $('<i class="fas fa-graduation-cap light-text clear"></i>');
 var bottom_section_title = $('<div class="section-title">');
 var bottom_header = $('<h2 class="clear">Education</h2>');
 var bottom_holder = $('<div class="col-md-12 bio">');
-var bottom_text = "My home office is decorated with Simpsons stuff. My biggest piece of memorabilia is a 6-foot tall fiberglass Homer Simpson.";
+var bottom_text = "";
 
 var instruction_holder = $('<div class="col-md-12 bio">');
 var instructions = $('<h4>Click an icon learn more about that program</h4>');
 
-function load() {
+function load(id) {
     top_button_holder.append(top_button);
     parent_div.append(top_button_holder);
     section_title.append(top_header);
@@ -47,13 +58,15 @@ function load() {
     instruction_holder.append(instructions);
     parent_div.append(instruction_holder);
     $("#info-space").append(parent_div);
+    // Store
+    localStorage.setItem(key, id);
 }
 greg();
-load();
+load("greg")
 //$("#greg").on("click", function(){
 $(document.body).on("click", "#greg", function () {
     greg();
-    load();
+    load("greg");
 });
 $(document.body).on("click", ".thumbnail-holder", function () {
     var id = $(this).attr("data_id");
@@ -74,7 +87,7 @@ $(document.body).on("click", ".thumbnail-holder", function () {
             mta();
         break;
     }
-    load();
+    load(id);
 });
 
 function greg(){
@@ -85,7 +98,7 @@ function greg(){
     top_text = "<table class='table'><tr><th>Position</th><th>Company</th><th></th></tr><tr><td>Sr Application Developer</td><td>Anthem</td><td>Mar 2000 - Present</td></tr><tr><td>Developer</td><td>B-Line Systems</td><td>1998 - 2000</td></tr><tr><td>Programer</td><td>Anheuser-Busch</td><td>1991 - 1998</td></tr></table>";
     mid_button = $('<i class="fas light-text fa-database clear icon-pro"></i>');
     mid_header = $('<h2 class="clear">Professional Skills</h2>');
-    mid_text = "<table class='table'><tr><td>.Net</td><td>javascript</td><td>jQuery</td></tr><tr><td>node.js</td><td>HTML</td><td>CSS</td></tr><tr><td>Unidata Database</td><td>Agile Development</td><td>Jira</td></tr>";
+    mid_text = "<table class='table'><tr><td>.Net</td><td>javascript</td><td>jQuery</td></tr><tr><td>node.js</td><td>HTML</td><td>CSS</td></tr><tr><td>Object Oriented Programming</td><td>Agile Development</td><td>Jira</td></tr>";
 
     bottom_button = $('<i class="fas light-text fa-graduation-cap clear"></i>');
     bottom_header = $('<h2 class="clear">Education</h2>');
@@ -100,8 +113,8 @@ function nhl(){
     top_text = "<h4>Hockey Hangman</h4>";
     mid_button = $('<i class="fas light-text fa-link clear"></i>');
     mid_header = $('<h2 class="clear">Links</h2>');
-    mid_text = '<div id="links"><div class="link-holder hangman" data_id = "nhl"><a href="https://gregorylanter.github.io/Word_Guess_Game/"><div class="thumbnail hockey"><div class="banner">Play</div></div></a></div>';
-    mid_text += '<div class="link-holder" data_id = "nhl"><a href="https://github.com/GregoryLanter/Word_Guess_Game"><div class="thumbnail git"><div class="banner">Git Repo</div></div></a></div></div>';
+    mid_text = '<div id="links"><div class="link-holder hangman" data_id = "nhl"><a href="https://gregorylanter.github.io/Word_Guess_Game/" target="_blank"><div class="thumbnail hockey"><div class="banner">Play</div></div></a></div>';
+    mid_text += '<div class="link-holder" data_id = "nhl"><a href="https://github.com/GregoryLanter/Word_Guess_Game" target="_blank"><div class="thumbnail git"><div class="banner">Git Repo</div></div></a></div></div>';
     
     bottom_button = $('<i class="fas light-text fa-clipboard clear"></i>');
     bottom_header = $('<h2 class="clear">Notes</h2>');
@@ -116,8 +129,8 @@ function bart(){
     mid_button = $('<i class="fas light-text fa-link clear"></i>');
     mid_header = $('<h2 class="clear">Links</h2>');
     
-    mid_text = '<div id="links"><div class="link-holder bart-rpg" data_id = "bart"><a href="https://gregorylanter.github.io/unit-4-game/"><div class="thumbnail rpg"><div class="banner">Play</div></div></a></div>';
-    mid_text += '<div class="link-holder" data_id = "bart"><a href="https://github.com/GregoryLanter/unit-4-game/settings"><div class="thumbnail git"><div class="banner">Git Repo</div></div></a></div>';
+    mid_text = '<div id="links"><div class="link-holder bart-rpg" data_id = "bart"><a href="https://gregorylanter.github.io/unit-4-game/" target="_blank"><div class="thumbnail rpg"><div class="banner">Play</div></div></a></div>';
+    mid_text += '<div class="link-holder" data_id = "bart"><a href="https://github.com/GregoryLanter/unit-4-game/" target="_blank"><div class="thumbnail git"><div class="banner">Git Repo</div></div></a></div>';
     
     bottom_button = $('<i class="fas light-text fa-clipboard clear"></i>');
     bottom_header = $('<h2 class="clear">Notes</h2>');
@@ -133,8 +146,8 @@ function beer(){
     mid_header = $('<h2 class="clear">Links</h2>');
     
     
-    mid_text = '<div id="links"><div class="link-holder beer-game" data_id = "beer"><a href="https://gregorylanter.github.io/TriviaGame/"><div class="thumbnail trivia"><div class="banner">Play</div></div></a></div>';
-    mid_text += '<div class="link-holder" data_id = "beer"><a href="https://github.com/GregoryLanter/TriviaGame"><div class="thumbnail git"><div class="banner">Git Repo</div></div></a></div>';
+    mid_text = '<div id="links"><div class="link-holder beer-game" data_id = "beer"><a href="https://gregorylanter.github.io/TriviaGame/" target="_blank"><div class="thumbnail trivia"><div class="banner">Play</div></div></a></div>';
+    mid_text += '<div class="link-holder" data_id = "beer"><a href="https://github.com/GregoryLanter/TriviaGame" target="_blank"><div class="thumbnail git"><div class="banner">Git Repo</div></div></a></div>';
     
     bottom_button = $('<i class="fas light-text fa-clipboard clear"></i>');
     bottom_header = $('<h2 class="clear">Notes</h2>');
@@ -151,8 +164,8 @@ function gif(){
     mid_header = $('<h2 class="clear">Links</h2>');
 
 
-    mid_text = '<div id="links"><div class="link-holder ajax" data_id = "gif"><a href="https://gregorylanter.github.io/Giphy-app/"><div class="thumbnail gifs"><div class="banner">Play</div></div></a></div>';
-    mid_text += '<div class="link-holder" data_id = "gif"><a href="https://github.com/GregoryLanter/Giphy-app"><div class="thumbnail git"><div class="banner">Git Repo</div></div></a></div>';
+    mid_text = '<div id="links"><div class="link-holder ajax" data_id = "gif"><a href="https://gregorylanter.github.io/Giphy-app/" target="_blank"><div class="thumbnail gifs"><div class="banner">Play</div></div></a></div>';
+    mid_text += '<div class="link-holder" data_id = "gif"><a href="https://github.com/GregoryLanter/Giphy-app" target="_blank"><div class="thumbnail git"><div class="banner">Git Repo</div></div></a></div>';
     
     bottom_button = $('<i class="fas light-text fa-clipboard clear"></i>');
     bottom_header = $('<h2 class="clear">Notes</h2>');
@@ -168,9 +181,8 @@ function mta(){
     mid_button = $('<i class="fas light-text fa-link clear"></i>');
     mid_header = $('<h2 class="clear">Links</h2>');
 
-//mid_text = '<div id="links"><div class="link-holder bart-rpg" data_id = "bart"><a href="https://gregorylanter.github.io/unit-4-game/"><div class="thumbnail rpg"><div class="banner">Play</div></div></a></div>';
-    mid_text = '<div id="links"><div class="link-holder train" data_id = "mta"><a href="https://gregorylanter.github.io/Train-Schedule/"><div class="thumbnail monster"><div class="banner">Play</div></div></a></div>';
-    mid_text += '<div class="link-holder" data_id = "mta"><a href="https://github.com/GregoryLanter/Train-Schedule"><div class="thumbnail git"><div class="banner">Git Repo</div></div></a></div>';
+    mid_text = '<div id="links"><div class="link-holder train" data_id = "mta"><a href="https://gregorylanter.github.io/Train-Schedule/" target="_blank"><div class="thumbnail monster"><div class="banner">Play</div></div></a></div>';
+    mid_text += '<div class="link-holder" data_id = "mta"><a href="https://github.com/GregoryLanter/Train-Schedule" target="_blank"><div class="thumbnail git"><div class="banner">Git Repo</div></div></a></div>';
     
     bottom_button = $('<i class="fas light-text fa-clipboard clear"></i>');
     bottom_header = $('<h2 class="clear">Notes</h2>');
