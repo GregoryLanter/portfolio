3a59e4cc3f7d4b9147bbcbbcf4df31cfe206c1de
Greg Lanter <dohnut22.gl@gmail.com>
2019-05-02 23:29:13 -0700

    updated for liri-node-app

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -59,6 +59,9 @@ $(document.body).on("click", "#greg", function () {
 $(document.body).on("click", ".thumbnail-holder", function () {
     var id = $(this).attr("data_id");
     switch (id) {
+        case "liri":
+            liri();
+            break;
         case "nhl":
             nhl();
         break;
@@ -96,6 +99,24 @@ function greg(){
     bottom_text = "Souther Illinois University Edwardsville - BA Computer Science 2002";
     instructions = $('<h6>Click an icon, on the right, to learn more about that program</h6>');
 }
+
+function liri(){
+    clear();
+    
+    top_button = $('<i class="fas fa-terminal light-text clear icon-2"></i>');
+    top_header = $('<h2 class="clear">Title</h2>');
+    top_text = "<h4>liri-node-app</h4>";
+    mid_button = $('<i class="fas light-text fa-link clear"></i>');
+    mid_header = $('<h2 class="clear">Links</h2>');
+    //mid_text = '<div id="links"><div class="link-holder liri" data_id = "nhl"><a href="https://github.com/GregoryLanter/liri-node-app" target="_blank"><div class="thumbnail liri-node"><div class="banner">Play</div></div></a></div>';
+    mid_text = '<div class="link-holder" data_id = "liri"><a href="https://github.com/GregoryLanter/liri-node-app" target="_blank"><div class="thumbnail git"><div class="banner">Git Repo</div></div></a></div></div>';
+    
+    bottom_button = $('<i class="fas light-text fa-clipboard clear icon-3"></i>');
+    bottom_header = $('<h2 class="clear">Notes</h2>');
+    bottom_text = "A node.js program to get info on bands, songs or movies.";
+    instructions = $('<h4>Click an icon learn more about that program</h4>');
+}
+
 
 function nhl(){
     clear();
